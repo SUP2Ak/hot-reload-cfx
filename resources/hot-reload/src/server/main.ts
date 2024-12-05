c2c0e034208816a795588785503bd0f10838e5bc
SUP2Ak <cormier.wesley@gmail.com>
2024-12-05 22:29:25 +0100

    ✨ feat(server): add hot reload server commands and events
    - implement resource start and stop logging for hot reload
    - add 'hot::status' command to check hot reload server status

--- resources/hot-reload/src/server/main.ts
+++ resources/hot-reload/src/server/main.ts
@@ -1,7 +1,16 @@
 import WATCHER_API from "./class/app";
 
-on('onResourceStart', (resourceName: string) => {
-    if (resourceName === GetCurrentResourceName()) {
-        console.log(`^3Hot Reload Server status: ${WATCHER_API.isRunning() ? '^2RUNNING' : '^1STOPPED'}^0`);
-    };
+on('onResourceStart', (resourceName: string): void => {
+    if (resourceName !== GetCurrentResourceName()) return;
+    console.log('^3Hot Reload Server started^0');
 });
+
+on('onResourceStop', (resourceName: string): void => {
+    if (resourceName !== GetCurrentResourceName()) return;
+    console.log('^3Hot Reload Server stopped^0');
+});
+
+RegisterCommand('hot::status', (source: number): void => {
+    if (source !== 0) return;
+    console.log(`^3Hot Reload Server status: ${WATCHER_API.isRunning() ? '^2RUNNING' : '^1STOPPED'}^0`);
+}, false);
