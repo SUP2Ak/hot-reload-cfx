bd3ff2c80b34a8ed7032584e8ce92c103e457e91
SUP2Ak <cormier.wesley@gmail.com>
2025-04-30 21:22:59 +0200

    ✨ feat(hot-reload): add commands for resource management
    - implement hot::add command for adding resources to watcher
    - implement hot::remove command for removing resources from watcher
    - update existing commands to use RESOURCE_NAME constant for clarity
    - add permissions documentation for new commands

--- resources/hot-reload/src/server/main.ts
+++ resources/hot-reload/src/server/main.ts
@@ -1,16 +1,36 @@
 import WATCHER_API from "./class/app";
+const RESOURCE_NAME = GetCurrentResourceName();
 
 on('onResourceStart', (resourceName: string): void => {
-    if (resourceName !== GetCurrentResourceName()) return;
+    if (resourceName !== RESOURCE_NAME) return;
     console.log('^3Hot Reload Server started^0');
 });
 
 on('onResourceStop', (resourceName: string): void => {
-    if (resourceName !== GetCurrentResourceName()) return;
+    if (resourceName !== RESOURCE_NAME) return;
     console.log('^3Hot Reload Server stopped^0');
 });
 
-RegisterCommand('hot::status', (source: number): void => {
-    if (source !== 0) return;
+RegisterCommand('hot::status', (_: number): void => {
     console.log(`^3Hot Reload Server status: ${WATCHER_API.isRunning() ? '^2RUNNING' : '^1STOPPED'}^0`);
-}, false);
+}, true);
+
+RegisterCommand('hot::add', (_: number, args: string[]): void => {
+    const resourceName = args[1];
+    if (!resourceName) return console.log('^1Usage: hot::add <resourceName> | <resourceName.path.ext>^0');
+    WATCHER_API.add(resourceName);
+}, true);
+
+RegisterCommand('hot::remove', (_: number, args: string[]): void => {
+    const resourceName = args[1];
+    if (!resourceName) return console.log('^1Usage: hot::remove <resourceName>^0');
+    WATCHER_API.remove(resourceName);
+}, true);
+
+/**
+ *  Ace Permissions:
+ * 
+ *  hot-reload.status
+ *  hot-reload.add
+ *  hot-reload.remove
+ */
